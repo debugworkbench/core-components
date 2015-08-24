7ccfd918b4ae8602bdce166579200e601d2083e5
Vadim Macagon <vadim.macagon@gmail.com>
2015-08-24 23:39:53 +0700

    Use the new file-input element in the debug-configuration element

--- debug-configuration/debug-configuration.ts
+++ debug-configuration/debug-configuration.ts
@@ -4,12 +4,21 @@ export interface IAuthorProperty {
   name: string;
   age: number;
 }
+import { FileInputElement } from '../file-input/file-input';
 
 /**
  * Base behavior of the DebugConfigurationElement.
  */
 @pd.is('debug-configuration')
 export class DebugConfigurationElement {
+  static register(): typeof DebugConfigurationElement {
+    // FIXME: Ugh... yeah, the dependencies are already referenced in the .html once, I don't like
+    // repeating myself, each dependency should be registered auto-magically when the .html is
+    // loaded.
+    FileInputElement.register();
+    return Polymer<typeof DebugConfigurationElement>(DebugConfigurationElement.prototype);
+  }
+  
   /** The returned object will only be valid after the element has been upgraded to a custom element. */
   get base(): polymer.Base {
     return <any> this;
