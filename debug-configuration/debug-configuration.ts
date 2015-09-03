9f1fc746947fa54581b2edddf4aa6bbe827d88d5
Vadim Macagon <vadim.macagon@gmail.com>
2015-09-04 00:14:27 +0700

    Simplify subscription to debug-configuration opened/closed events
    Now you don't have to remember some obscure event names to bind event
    handlers to the element.

--- debug-configuration/debug-configuration.ts
+++ debug-configuration/debug-configuration.ts
@@ -1,6 +1,8 @@
 import * as pd from 'polymer-ts-decorators';
 import { IDebugConfigElementBehavior, IDebugConfig } from '../lib/debug-engine';
 import { createElement } from '../lib/debug-workbench';
+import { Disposable } from 'event-kit';
+import addDisposableListener from '../lib/disposable-dom-event-listener';
 
 interface ILocalDOM {
   dialog: PolymerElements.PaperDialog;
@@ -18,7 +20,17 @@ export default class DebugConfigurationElement implements IDebugConfigElementBeh
   static create(debugConfig: IDebugConfig): Promise<IDebugConfigurationElement> {
 	  return createElement((<any> DebugConfigurationElement.prototype).is, debugConfig);
   }
-
+  
+  /** Add a listener to be called when the dialog is opened. */
+  onOpened(callback: EventListener): Disposable {
+    return addDisposableListener($(this).dialog, 'iron-overlay-opened', callback);
+  }
+  
+  /** Add a listener to be called when the dialog is closed. */
+  onClosed(callback: EventListener): Disposable {
+    return addDisposableListener($(this).dialog, 'iron-overlay-closed', callback);
+  }
+  
   open(): void {
     const dialog = $(this).dialog;
     if (dialog) {
