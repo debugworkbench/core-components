3df8b5cb1fc83048cd2a6765d3322c85b59321cd
Vadim Macagon <vadim.macagon@gmail.com>
2015-09-10 11:33:48 +0700

    Add index.d.ts to polymer-elements typings
    The index file includes all the individual element declarations to make
    it easier to consume the typings in packages that depend on this one.
    To streamline this use case the index file is also referenced in
    package.json so that running `tsd link` in a package that depends on
    this one will automatically add a reference to polymer-elements index
    file to the .d.ts file managed by tsd.

---
+++ typings/polymer-elements/index.d.ts
@@ -0,0 +1,6 @@
+/// <reference path="./iron-selector.d.ts" />
+/// <reference path="./paper-dialog.d.ts" />
+/// <reference path="./paper-dropdown-menu.d.ts" />
+/// <reference path="./paper-icon-button.d.ts" />
+/// <reference path="./paper-input.d.ts" />
+/// <reference path="./paper-item.d.ts" />
