facf91e812d95dc193c94d3671a0b7b2b0acdaf5
Vadim Macagon <vadim.macagon@gmail.com>
2015-10-02 22:31:25 +0700

    Add options parameter to INotificationPresenter methods

--- src/notification-presenter.ts
+++ src/notification-presenter.ts
@@ -3,14 +3,22 @@
 
 import { Disposable } from 'event-kit';
 
+export interface INotificationOptions {
+  detail?: string;
+}
+
+export interface IErrorNotificationOptions extends INotificationOptions {
+  stack?: string;
+}
+
 /** Displays notifications to the user. */
 export interface INotificationPresenter {
   /** Inform the user that an operation was completed successfully. */
-  success(message: string): void;
+  success(message: string, options?: INotificationOptions): void;
   /** Display some useful information to the user. */
-  info(message: string): void;
+  info(message: string, options?: INotificationOptions): void;
   /** Display a warning to the user. */
-  warning(message: string): void;
+  warning(message: string, options?: INotificationOptions): void;
   /** Inform the user that an error occurred. */
-  error(message: string): void;
+  error(message: string, options?: IErrorNotificationOptions): void;
 }
